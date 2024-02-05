1919f974c77b9e6e1eea8b7003866bb3a6ceb475
katyazzzh <madam.zheleznova@icloud.com>
2024-02-05 15:25:15 +0300

    lesson 3: fixed controller access

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyPlaceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'hello';
 });
+
+Route::get('/my_page', [MyPlaceController::class,'index']);
