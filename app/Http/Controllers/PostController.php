741e34679988b7ae4d6e555430ce737a621dab2a
katyazzzh <madam.zheleznova@icloud.com>
2024-02-06 13:18:50 +0300

    lesson 9: added route and function to create posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,29 @@ class PostController extends Controller
         $post = Post::where('is_published', 1)->first();
         dd($post->title);
     }
+
+    public function create() {
+        $postsArr = [
+            [
+                'title' => 'first post from php',
+                'content' => 'first post content from php',
+                'image' => 'php',
+                'likes' => 20,
+                'is_published' => 1,
+            ],
+            [
+                'title' => 'second post from php',
+                'content' => 'second post content from php',
+                'image' => 'php2',
+                'likes' => 15,
+                'is_published' => 1,
+            ]
+        ];
+
+        foreach ($postsArr as $post) {
+            dump($post['title']);
+            Post::create($post);
+            dump('created');
+        }
+    }
 }
