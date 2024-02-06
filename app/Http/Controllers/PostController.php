ac475821bf3da17d6173ecac5d3586ead49b1d32
katyazzzh <madam.zheleznova@icloud.com>
2024-02-06 12:53:42 +0300

    lesson 8: show title of first post where is_published=true

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::where('is_published', 1)->get();
-        foreach ($posts as $post) {
-            dump($post->title);
-        }
+        $post = Post::where('is_published', 1)->first();
+        dd($post->title);
     }
 }
