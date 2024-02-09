0e9db3ab07b8aa73faf8fbf2c9067c056346cb06
katyazzzh <madam.zheleznova@icloud.com>
2024-02-09 15:04:42 +0300

    lesson 19: added tags function to Post model to show all tags of some post and same function posts to Tag model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
     public function index()
     {
-        // $category = Category::find(1);
-        // dd($category->posts);
+//         $category = Category::find(1);
+//         dd($category->posts);
 
 //        $post = Post::find(1);
 //        dd($post->category);
+
+//        $tag = Tag::find(1);
+//        dd($tag->posts);
+
+//        $post = Post::find(3);
+//        dd($post->tags);
+
         $posts = Post::all();
         return view('post.index', compact('posts'));
     }
