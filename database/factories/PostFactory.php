d3e07780454a65ce8ff6b643488ac1f80dabf510
katyazzzh <madam.zheleznova@icloud.com>
2024-02-16 17:15:23 +0300

    lesson 28: created factories for posts, categories and tags, added db seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle,
+            'content' => $this->faker->text,
+            'image' => $this->faker->imageUrl,
+            'likes' => random_int(1,1000),
+            'is_published' => 1,
+            'category_id' => Category::get()->random()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(10)->create();
+        $tags = Tag::factory(20)->create();
+        $posts = Post::factory(30)->create();
+        foreach ($posts as $post) {
+            //5 рандомных тегов, у них берется ID, все сохраняется в коллекцию $tagsId
+            $tagsId = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsId);
+        }
     }
 }
