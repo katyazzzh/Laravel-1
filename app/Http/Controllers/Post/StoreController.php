9433efa087ac51e7173e1945565dc1c305589564
katyazzzh <madam.zheleznova@icloud.com>
2024-02-16 16:15:40 +0300

    lesson 26: added store and update requests

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
+use Illuminate\Session\Store;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => ''
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -3,19 +3,14 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
-        $newData = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => ''
-        ]);
+        $newData = $request->validated();
         $tags = $newData['tags'];
         unset($newData['tags']);
 
