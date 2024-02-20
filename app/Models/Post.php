b212493447654089e5cd38e3ddf07cd5c411fc09
katyazzzh <madam.zheleznova@icloud.com>
2024-02-20 10:29:51 +0300

    lesson 30: created post filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,8 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Post extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, Filterable;
     protected $table = 'posts';
     protected $guarded = [];
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,7 +6,7 @@
         @endforeach
 
         <div class="mt-3">
-            {{$posts->links()}}
+            {{$posts->withQueryString()->links()}}
         </div>
     </div>
     <div>
